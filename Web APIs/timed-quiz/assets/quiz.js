8362aad4e003c7935ec256f4390cde25af79b470
Jesús Posada <jesussernauni@gmail.com>
2024-03-11 18:48:47 -0500

    Timer and transitions betwween screens

--- Web APIs/timed-quiz/assets/quiz.js
+++ Web APIs/timed-quiz/assets/quiz.js
@@ -1,57 +1,173 @@
 (function () {
-    /* 
-    * We  can code here also
-    */
 
-    document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function (screen, buttons) {
         const screensDiv = document.getElementById('screens');
         
-        screensDiv.innerHTML = screens.screen1();
+        screensDiv.innerHTML = screens.startScreen();
     }, false);
 })();
 
 const screens =  {
-    screen1: function () {
+    startScreen: function () {
         return `
             <div id="screen1">
-                <button id="button-screen1" onclick="buttons.buttonScreen1(this)"> hello world </button>
+                <h1>Coding quiz challenge</h1>
+                <p id="description"> description </p>
+                <button id="button-screen1" onclick="buttons.start(this)"> Start quiz! </button>
             </div>
         `
     },
-    screen2: function () {
+    quizScreen1: function () {
         return `
             <div>
-                <button id="button-screen2" onclick="buttons.buttonScreen2(this)"> hello world </button>
+                <button id="button-screen2" onclick="buttons.buttonqScreen2(this)"> hello world </button>
             </div>
         `
     },
-    screen3: function () {
+    quizScreen2: function () {
         return `
             <div>
-                <p> Hello from screen 3</p>
+                <button id="button-screen2" onclick="buttons.buttonqScreen3(this)"> hello world </button>
             </div>
         `
-    }
+    },
+    quizScreen3: function () {
+        return `
+            <div>
+                <button id="button-screen2" onclick="buttons.buttonqScreen4(this)"> hello world </button>
+            </div>
+        `
+    },
+    quizScreen4: function () {
+        return `
+            <div>
+                <button id="button-screen2" onclick="buttons.buttonqScreen5(this)"> hello world </button>
+            </div>
+        `
+    },
+    quizScreen5: function () {
+        return `
+            <div>
+                <button id="button-screen2" onclick="buttons.buttonScoreScreen(this)"> hello world </button>
+            </div>
+        `
+    },
+    scoreScreen: function () {
+        return `
+            <div>
+                <p>Score Screen!!</p>
+                <input id="user-name" type="text" name="name">
+                <button id="button-screen2" onclick="buttons.buttonHighScoresScreen(this)"> hello world </button>
+            </div>
+        `
+    },
+    highScoresScreen: function() {
+        console.log(JSON.parse(localStorage.getItem('highscores')));
+        return `
+            <div>
+                <p>High scores!!</p>
+                <button id="button-screen2" onclick="buttons.buttonGoToStartScreen(this)"> Go back!</button>
+            </div>
+        `
+    },
 }
+
+let timer = 0
+const startTimer = function (screens, screen) {
+    setInterval(function () {
+        if (timer > 0) {
+            document.getElementById('countdown').innerHTML = timer
+            timer--;
+        }
+        if (timer  == 74) {
+            screens.innerHTML = screen();
+        }
+    }, 1000);
+    
+};
 
 const buttons = {
     screens: screens,
-    buttonScreen1: function (btn) {
+    start: function (btn) {
         if (btn) {
-    
-            const screen1 = btn.parentNode;
-            const screens = screen1.parentNode;
-    
-            screens.innerHTML = this.screens.screen2();
+            btn.disabled = true;
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+
+            timer = 75;
+            startTimer(screens, this.screens.quizScreen1);
         }
     },
-    buttonScreen2: function (btn) {
+    buttonqScreen2: function (btn) {
         if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
     
-            const screen1 = btn.parentNode;
-            const screens = screen1.parentNode;
+            screens.innerHTML = this.screens.quizScreen2();
+        }
+    },
+    buttonqScreen3: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
     
-            screens.innerHTML = this.screens.screen3();
+            screens.innerHTML = this.screens.quizScreen3();
+        }
+    },
+    buttonqScreen4: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+    
+            screens.innerHTML = this.screens.quizScreen4();
+        }
+    },
+    buttonqScreen5: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+    
+            screens.innerHTML = this.screens.quizScreen5();
+        }
+    },
+    buttonScoreScreen: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+            timer = 0;
+            
+            screens.innerHTML = this.screens.scoreScreen();
+        }
+    },
+    buttonHighScoresScreen: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+
+            const highscores = JSON.parse(localStorage.getItem('highscores'));
+            
+            const name = document.getElementById('user-name');
+            const score = document.getElementById('countdown');
+            console.log(name, score);
+            highscores.push({
+                name: name.value,
+                score: score.textContent
+            });
+            localStorage.setItem('highscores', JSON.stringify(highscores))
+
+            
+            screens.innerHTML = this.screens.highScoresScreen();
+        }
+    },
+    buttonGoToStartScreen: function (btn) {
+        if (btn) {
+            const screen = btn.parentNode;
+            const screens = screen.parentNode;
+            
+            screens.innerHTML = this.screens.startScreen();
         }
     }
 }
+
+const highscores = [];
+localStorage.setItem('highscores', JSON.stringify(highscores))
